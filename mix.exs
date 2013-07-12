c0013273060fd11514fe7706e7d5726d4bc0db77
Sofia Velmer <pyth0n3r@yandex.ru>
2013-07-12 05:07:22 +0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,8 +3,10 @@ defmodule Algorithms.Mixfile do
 
   def project do
     [ app: :algorithms,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 0.9.4-dev",
+      source_url: "https://github.com/valmet/algorithms",
+
       deps: deps ]
   end
 
@@ -16,6 +18,9 @@ defmodule Algorithms.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    []
+    [
+    #  { :benchmark, git: "https://github.com/meh/elixir-benchmark.git" },
+    #  { :ex_doc, github: "elixir-lang/ex_doc" },
+    ]
   end
 end
