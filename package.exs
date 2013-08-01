317ac60e13b711c8f244cc40c634e96835fa0228
Sofia Velmer <me@require.pm>
2013-08-01 11:11:11 +0400

    expm.co package

---
+++ package.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "algorithms", description: "Collection of algorithms for your Elixirs",
+                 version: "0.0.3", keywords: ["algorithms", "sorting", "search", "graph"], 
+                 maintainers: [[name: "Makise Kurisu", 
+                                email: "me@require.pm"]],
+                 repositories: [[github: "valmet/algorithms"]])
+    
